da846f2c6ef76611302e55d8fbe777c63b71e7f1
Rahul gurjar <rahulgurjar824582@gmail.com>
2025-09-08 00:57:54 +0530

    Enhance API configuration by including credentials in base query

--- src/store/api/baseApi.ts
+++ src/store/api/baseApi.ts
@@ -1,12 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../index';
 
-console.log('Environment variable:', import.meta.env.VITE_BACKEND_URL);
 const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1';
-console.log('Final Backend URL:', backendUrl);
 
 const baseQuery = fetchBaseQuery({
   baseUrl: backendUrl,
+  credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
